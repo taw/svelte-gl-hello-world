396e50ebafb31306f82de7014cac7fd91d77effe
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2020-08-14 15:02:17 +0100

    Fixed spacing

--- src/App.svelte
+++ src/App.svelte
@@ -12,19 +12,19 @@
   const light = {};
 
   onMount(() => {
-  	let frame;
+    let frame;
 
-  	const loop = () => {
-  		frame = requestAnimationFrame(loop);
+    const loop = () => {
+      frame = requestAnimationFrame(loop);
 
-  		light.x = 3 * Math.sin(Date.now() * 0.001);
-  		light.y = 2.5 + 2 * Math.sin(Date.now() * 0.0004);
-  		light.z = 3 * Math.cos(Date.now() * 0.002);
-  	};
+      light.x = 3 * Math.sin(Date.now() * 0.001);
+      light.y = 2.5 + 2 * Math.sin(Date.now() * 0.0004);
+      light.z = 3 * Math.cos(Date.now() * 0.002);
+    };
 
-  	loop();
+    loop();
 
-  	return () => cancelAnimationFrame(frame);
+    return () => cancelAnimationFrame(frame);
   });
 </script>
 
@@ -32,7 +32,7 @@
   <GL.Target id="center" location={[0, h/2, 0]}/>
 
   <GL.OrbitControls maxPolarAngle={Math.PI / 2} let:location>
-  	<GL.PerspectiveCamera {location} lookAt="center" near={0.01} far={1000}/>
+    <GL.PerspectiveCamera {location} lookAt="center" near={0.01} far={1000}/>
   </GL.OrbitControls>
 
   <GL.AmbientLight intensity={0.3}/>
@@ -40,80 +40,80 @@
 
   <!-- floor -->
   <GL.Mesh
-  	geometry={GL.plane()}
-  	location={[0,-0.01,0]}
-  	rotation={[-90,0,0]}
-  	scale={10}
-  	uniforms={{ color: 0xffffff }}
+    geometry={GL.plane()}
+    location={[0,-0.01,0]}
+    rotation={[-90,0,0]}
+    scale={10}
+    uniforms={{ color: 0xffffff }}
   />
 
   <GL.Mesh
-  	geometry={GL.box()}
-  	location={[0,h/2,0]}
-  	rotation={[0,-20,0]}
-  	scale={[w,h,d]}
-  	uniforms={{ color: from_hex(color) }}
+    geometry={GL.box()}
+    location={[0,h/2,0]}
+    rotation={[0,-20,0]}
+    scale={[w,h,d]}
+    uniforms={{ color: from_hex(color) }}
   />
 
   <!-- spheres -->
   <GL.Mesh
-  	geometry={GL.sphere({ turns: 36, bands: 36 })}
-  	location={[-0.5, 0.4, 1.2]}
-  	scale={0.4}
-  	uniforms={{ color: 0x123456, alpha: 0.9 }}
-  	transparent
+    geometry={GL.sphere({ turns: 36, bands: 36 })}
+    location={[-0.5, 0.4, 1.2]}
+    scale={0.4}
+    uniforms={{ color: 0x123456, alpha: 0.9 }}
+    transparent
   />
 
   <GL.Mesh
-  	geometry={GL.sphere({ turns: 36, bands: 36 })}
-  	location={[-1.4, 0.6, 0.2]}
-  	scale={0.6}
-  	uniforms={{ color: 0x336644, alpha: 0.9 }}
-  	transparent
+    geometry={GL.sphere({ turns: 36, bands: 36 })}
+    location={[-1.4, 0.6, 0.2]}
+    scale={0.6}
+    uniforms={{ color: 0x336644, alpha: 0.9 }}
+    transparent
   />
 
   <!-- moving light -->
   <GL.Group location={[light.x,light.y,light.z]}>
-  	<GL.Mesh
-  		geometry={GL.sphere({ turns: 36, bands: 36 })}
-  		location={[0,0.2,0]}
-  		scale={0.1}
-  		uniforms={{ color: 0xffffff, emissive: 0xcccc99 }}
-  	/>
+    <GL.Mesh
+      geometry={GL.sphere({ turns: 36, bands: 36 })}
+      location={[0,0.2,0]}
+      scale={0.1}
+      uniforms={{ color: 0xffffff, emissive: 0xcccc99 }}
+    />
 
-  	<GL.PointLight
-  		location={[0,0,0]}
-  		color={0xffffff}
-  		intensity={0.6}
-  	/>
+    <GL.PointLight
+      location={[0,0,0]}
+      color={0xffffff}
+      intensity={0.6}
+    />
   </GL.Group>
 </GL.Scene>
 
 <div class="controls">
   <label>
-  	<input type="color" style="height: 40px" bind:value={color}>
+    <input type="color" style="height: 40px" bind:value={color}>
   </label>
 
   <label>
-  	<input type="range" bind:value={w} min={0.1} max={5} step={0.1}> width ({w})
+    <input type="range" bind:value={w} min={0.1} max={5} step={0.1}> width ({w})
   </label>
 
   <label>
-  	<input type="range" bind:value={h} min={0.1} max={5} step={0.1}> height ({h})
+    <input type="range" bind:value={h} min={0.1} max={5} step={0.1}> height ({h})
   </label>
 
   <label>
-  	<input type="range" bind:value={d} min={0.1} max={5} step={0.1}> depth ({d})
+    <input type="range" bind:value={d} min={0.1} max={5} step={0.1}> depth ({d})
   </label>
 </div>
 
 <style>
   .controls {
-  	position: absolute;
-  	top: 1em;
-  	left: 1em;
-  	background-color: rgba(255,255,255,0.7);
-  	padding: 1em;
-  	border-radius: 2px;
+    position: absolute;
+    top: 1em;
+    left: 1em;
+    background-color: rgba(255,255,255,0.7);
+    padding: 1em;
+    border-radius: 2px;
   }
 </style>
